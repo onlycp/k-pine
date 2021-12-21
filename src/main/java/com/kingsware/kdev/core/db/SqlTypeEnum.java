b3757e1751cb108c2f47e4dfc5523b8ca51cc29b
陈鹏 <chenp@kingsware.cn>
2021-12-21 18:04:39 +0800

    初步生成SQL工具类

---
+++ src/main/java/com/kingsware/kdev/core/db/SqlTypeEnum.java
@@ -0,0 +1,18 @@
+package com.kingsware.kdev.core.db;
+
+/**
+ * SQL语句类型
+ *
+ * @author chen peng
+ * @version 1.0.0
+ * @date 2021/12/21 2:10 下午
+ */
+public enum SqlTypeEnum {
+    // 插入
+    INSERT,
+    // 更新
+    UPDATE,
+    // 删除
+    DELETE;
+
+}
