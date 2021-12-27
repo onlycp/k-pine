d680a1d5ad7a26b7fb8368658cb2622c1348fdfe
AndyZheng <929031959@qq.com>
2021-12-27 10:52:53 +0800

    提交字典model

--- src/main/java/com/kingsware/kdev/sys/model/SysDict.java
+++ src/main/java/com/kingsware/kdev/sys/model/SysDict.java
@@ -1,5 +1,6 @@
 package com.kingsware.kdev.sys.model;
 
+import com.kingsware.kdev.core.bean.BaseManageModel;
 import com.kingsware.kdev.core.bean.BaseModel;
 import com.kingsware.kdev.core.orm.annotation.AutoEnum;
 import com.kingsware.kdev.core.orm.annotation.Column;
@@ -12,7 +13,7 @@ import com.kingsware.kdev.core.orm.annotation.Table;
  * @date 2021-12-27 10:20
  */
 @Table
-public class SysDict extends BaseModel {
+public class SysDict extends BaseManageModel {
 
     /** 字典名 */
     private String name;
--- src/main/java/com/kingsware/kdev/sys/model/SysDictItem.java
+++ src/main/java/com/kingsware/kdev/sys/model/SysDictItem.java
@@ -1,5 +1,6 @@
 package com.kingsware.kdev.sys.model;
 
+import com.kingsware.kdev.core.bean.BaseManageModel;
 import com.kingsware.kdev.core.bean.BaseModel;
 import com.kingsware.kdev.core.orm.annotation.AutoEnum;
 import com.kingsware.kdev.core.orm.annotation.Column;
@@ -12,7 +13,7 @@ import com.kingsware.kdev.core.orm.annotation.Table;
  * @date 2021-12-27 10:20
  */
 @Table
-public class SysDictItem extends BaseModel {
+public class SysDictItem extends BaseManageModel {
 
     /** 字典名 */
     private String name;
