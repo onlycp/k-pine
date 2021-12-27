8ad54bbea2bb5b1764f273e9515a0f953f310938
AndyZheng <929031959@qq.com>
2021-12-27 10:43:47 +0800

    提交字典model

---
+++ src/main/java/com/kingsware/kdev/sys/model/to/SysDictTO.java
@@ -0,0 +1,54 @@
+package com.kingsware.kdev.sys.model.to;
+
+import com.kingsware.kdev.core.bean.BaseModel;
+import com.kingsware.kdev.core.orm.annotation.Table;
+
+/**
+ * @author AndyZheng
+ * @version 1.0.0
+ * @description: 字典类型
+ * @date 2021-12-27 10:20
+ */
+public class SysDictTO {
+
+    /** id */
+    private String id;
+    /** 字典名 */
+    private String name;
+    /** 字典代码 */
+    private String code;
+    /** 备注 */
+    private String note;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
