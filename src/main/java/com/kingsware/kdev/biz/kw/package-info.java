cc081260882b39d79fc7436cc4cd5d335ad1e229
陈鹏 <chenp@kingsware.cn>
2022-01-04 10:29:05 +0800

    增加业务系统的相关包

---
+++ src/main/java/com/kingsware/kdev/biz/kw/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 资金管理系统
+ *
+ * @author chen peng
+ * @version 1.0.0
+ * @date 2022/1/4 10:12 上午
+ */
+package com.kingsware.kdev.biz.kw;
---
+++ src/main/java/com/kingsware/kdev/biz/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 业务系统顶级包
+ *
+ * @author chen peng
+ * @version 1.0.0
+ * @date 2022/1/4 9:59 上午
+ */
+package com.kingsware.kdev.biz;
