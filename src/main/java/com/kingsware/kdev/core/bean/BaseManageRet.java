046c7d242ad67aa524ef0dceb96ceab92d22eabd
AndyZheng <929031959@qq.com>
2022-01-06 14:47:19 +0800

    baseManageRet添加JasonFormat

--- src/main/java/com/kingsware/kdev/core/bean/BaseManageRet.java
+++ src/main/java/com/kingsware/kdev/core/bean/BaseManageRet.java
@@ -1,5 +1,6 @@
 package com.kingsware.kdev.core.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -19,9 +20,11 @@ public class BaseManageRet extends BaseSimpleRet{
     /** 创建人员 **/
     private String whoCreated;
     /** 创建时间 **/
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss", timezone="GMT+8")
     private Timestamp whenCreated;
     /** 修改人员 **/
     private String whoModified;
     /** 修改时间 **/
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss", timezone="GMT+8")
     private Timestamp whenModified;
 }
