c307b11d38851a5ac17b9b609f428d818a67a2b0
AndyZheng <929031959@qq.com>
2022-05-26 20:23:51 +0800

    ADD:数据表字段新增SQL

---
+++ initSql/Mysql/version_4_1.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `dev_application_version_history` ADD COLUMN `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文件名' AFTER `version`;
+
+ALTER TABLE `dev_application_version_history` ADD COLUMN `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注' AFTER `file_name`;
+
+ALTER TABLE `dev_application_version_history` ADD COLUMN `export_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '导出数据参数' AFTER `note`;
+
+ALTER TABLE `sys_logic_flow` ADD COLUMN `default_source_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '默认数据源' AFTER `app_id`;
