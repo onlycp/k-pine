db4f454ef38dc396f9ba37a868832da8f71f1bdc
陈鹏 <chenp_self@126.com>
2023-02-17 09:46:18 +0800

    MODIFY: 处理FAAS通道插件

---
+++ k-dev-core/src/main/java/com/kingsware/kdev/core/plugins/FaasChannelPlugin.java
@@ -0,0 +1,23 @@
+package com.kingsware.kdev.core.plugins;
+
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * 内容存储插件
+ */
+public interface FaasChannelPlugin {
+
+
+    /**
+     * 发送
+     * @return  保存类型
+     */
+    String send(String apiUrl, String body, Map<String, String> headerMap);
+
+    /**
+     * 获取通道名称
+     * @return
+     */
+    String name();
+}
