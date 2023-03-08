6479ea37e44e6f39544003eb20e361ce8b42f143
chenpeng <chenpeng@z.cn>
2023-03-08 17:35:15 +0800

    MODIFY: 将sql移到resources模板

--- k-dev-app/src/main/resources/initSql/MySql/version_34_1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE sys_search_config ADD search_columns text NULL COMMENT '搜索字段中文';
-ALTER TABLE sys_search_config MODIFY COLUMN `columns` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '搜索字段';
