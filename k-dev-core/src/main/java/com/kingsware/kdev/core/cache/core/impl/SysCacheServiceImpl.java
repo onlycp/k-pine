3e50618c765c9108b9cf5f5c471c633952c33f20
chenpeng <chenp.bh@gmail.com>
2024-07-22 16:58:56 +0800

    MODIFY: 1. 缓存采用kutils的

--- k-dev-core/src/main/java/com/kingsware/kdev/core/cache/core/impl/SysCacheServiceImpl.java
+++ k-dev-core/src/main/java/com/kingsware/kdev/core/cache/core/impl/SysCacheServiceImpl.java
@@ -3,6 +3,7 @@ package com.kingsware.kdev.core.cache.core.impl;
 import com.kingsware.kdev.core.cache.core.SysCacheService;
 import com.kingsware.kdev.core.model.SysCache;
 import com.kingsware.kdev.core.orm.DB;
+import com.kingsware.kdev.core.util.FaasInvoke;
 import com.kingsware.kdev.core.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,30 +22,13 @@ public class SysCacheServiceImpl implements SysCacheService {
     private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss:SSS");
 
     @Override
-    public SysCache getCache(String key) {
-        SysCache sysCache = DB.findOne(SysCache.class, GET_SQL, key);
-        if (sysCache != null && StringUtils.isNotEmpty(sysCache.getWhenExpired())) {
-            try {
-                // 如果过期，取到的值为空
-                LocalDateTime whenExpired = LocalDateTime.parse(sysCache.getWhenExpired(), FORMATTER);
-                if (whenExpired.isBefore(LocalDateTime.now())) {
-                    // 过期自动删除
-                    removeCache(key);
-                    sysCache = null;
-                }
-            } catch (Exception e) {
-                log.error("获取缓存数据失败，日期格式有误：", e);
-                sysCache = null;
-            } finally {
-                return sysCache;
-            }
-        }
-        return sysCache;
+    public String getCache(String key) {
+        return FaasInvoke.getCache(key);
     }
 
     @Override
     public void setCache(String key, String value) {
-        this.setCache(key, value, 1800);
+        FaasInvoke.setCache(key, value);
     }
 
     /**
@@ -55,40 +39,12 @@ public class SysCacheServiceImpl implements SysCacheService {
      */
     @Override
     public void setCache(String key, String value, long expireTime) {
-        try {
-            SysCache sysCache = DB.findOne(SysCache.class, GET_SQL, key);
-            LocalDateTime currentTime = LocalDateTime.now();
-            currentTime = currentTime.plusSeconds(expireTime);
-            String currentTimeStr = expireTime == -1 ? null : currentTime.format(FORMATTER);
-            boolean isNew = false;
-            if (sysCache == null) {
-                sysCache = new SysCache();
-                sysCache.setCode(key);
-                isNew = true;
-            }
-            sysCache.setValue(value);
-            sysCache.setWhenExpired(currentTimeStr);
-            sysCache.setWhenCreated(LocalDateTime.now().format(FORMATTER));
-
-            if (isNew) {
-                DB.save(sysCache);
-            } else {
-                DB.update(sysCache);
-            }
-
-        } catch (Exception e) {
-            log.error("缓存数据失败：", e);
-        }
+       FaasInvoke.setCache(key, value, expireTime*1000);
     }
 
     @Override
     public void removeCache(String key) {
-        DB.executeUpdateSql(DELETE_SQL, key);
-    }
-
-    @Override
-    public List<SysCache> listCache() {
-        return DB.findList(SysCache.class, LIST_SQL);
+        FaasInvoke.removeCache(key);
     }
 
 }
---
+++ k-dev-core/src/main/java/com/kingsware/kdev/core/util/FaasInvoke.java
@@ -0,0 +1,77 @@
+package com.kingsware.kdev.core.util;
+
+import com.kingsware.kdev.core.orm.DB;
+import com.kingsware.kdev.core.orm.kdb.KdbRet;
+
+public class FaasInvoke {
+
+    private static String privateKey = "009308ea577dad81c8ef52b5b98c9ed80d0aedef8256914cd31708b6a470d313c3";
+    private static String publicKey = "0492073b8f6b9efed4fc75ce6f9dbf0359c451e3d128fff000d5414d6cf50304c71f326854f024b00584abd999a6c0fabb10c95ee21658f5b186678b7c9614f97f";
+
+    public static String loginDecrypt(String data) throws Exception {
+        String script = String.format("kutils.sm2Decode('%s', '%s', '%s');", data, privateKey, publicKey);
+        KdbRet<String> ret = DB.kdbApi().executeScript(script);
+        return ret.getResponseBody();
+    }
+
+    public static String loginEncrypt(String data) throws Exception {
+        String script = String.format("kutils.sm2Encode('%s', '%s', '%s');", data, privateKey, publicKey);
+        KdbRet<String> ret = DB.kdbApi().executeScript(script);
+        return ret.getResponseBody();
+    }
+
+    /**
+     * 从缓存中获取指定键的值。
+     *
+     * @param key 缓存中的键。
+     * @return 键对应的值，如果键不存在或缓存已过期，则可能返回空字符串。
+     */
+    public static String getCache(String key) {
+        // 构造调用KDB缓存获取脚本的字符串
+        String script = String.format("kutils.getCache('k-pine-app','%s');", key);
+        // 执行脚本并返回结果
+        KdbRet<String> ret = DB.kdbApi().executeScript(script);
+        // 返回脚本执行的结果
+        return ret.getResponseBody();
+    }
+
+    /**
+     * 设置缓存项，带过期时间。
+     *
+     * @param key 缓存中的键。
+     * @param value 缓存中的值。
+     * @param expire 缓存项的过期时间，以秒为单位。
+     */
+    public static void setCache(String key, String value, Long expire) {
+        // 构造调用KDB缓存设置脚本的字符串，包括过期时间
+        String script = String.format("kutils.setCache('k-pine-app','%s','%s', %d);", key, value, expire);
+        // 执行脚本
+        DB.kdbApi().executeScript(script);
+    }
+
+    /**
+     * 设置缓存项，不带过期时间。
+     *
+     * @param key 缓存中的键。
+     * @param value 缓存中的值。
+     */
+    public static void setCache(String key, String value) {
+        // 构造调用KDB缓存设置脚本的字符串，不包括过期时间
+        String script = String.format("kutils.setCache('k-pine-app','%s','%s');", key, value);
+        // 执行脚本
+        DB.kdbApi().executeScript(script);
+    }
+
+    /**
+     * 从缓存中移除指定的键。
+     *
+     * @param key 要移除的缓存键。
+     */
+    public static void removeCache(String key) {
+        // 构造调用KDB缓存移除脚本的字符串
+        String script = String.format("kutils.removeCache('k-pine-app','%s');", key);
+        // 执行脚本
+        DB.kdbApi().executeScript(script);
+    }
+
+}
