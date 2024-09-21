0abbe4deee3131c60ef6ad30f8eb72358d24067b
陈鹏 <chenp_self@126.com>
2024-09-21 16:38:49 +0800

    1、提交缺少文件
    (cherry picked from commit d748cef9e2bbf6fd9a6a84ff890b40307d4ba390)

---
+++ k-dev-core/src/main/java/com/kingsware/kdev/core/bean/Option.java
@@ -0,0 +1,19 @@
+package com.kingsware.kdev.core.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author chen peng
+ * @version 1.0.0
+ * @date 2024/9/18 17:48
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Option {
+    private String label;
+    private String value;
+}
