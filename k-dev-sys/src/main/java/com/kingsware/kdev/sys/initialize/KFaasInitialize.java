8cc117f544b366f9366a192742c929f3c5863351
陈鹏 <chenp_self@126.com>
2024-12-02 11:31:44 +0800

    db.json支持国密4加密

--- k-dev-sys/src/main/java/com/kingsware/kdev/sys/initialize/KFaasInitialize.java
+++ k-dev-sys/src/main/java/com/kingsware/kdev/sys/initialize/KFaasInitialize.java
@@ -4,6 +4,7 @@ import com.kingsware.kdev.core.base.SystemInitialize;
 import com.kingsware.kdev.core.bean.JdbcUrl;
 import com.kingsware.kdev.core.cache.license.LicenseManager;
 import com.kingsware.kdev.core.context.SpringContext;
+import com.kingsware.kdev.core.encrypt.SM4Utils;
 import com.kingsware.kdev.core.kflow.define.FlowDefinition;
 import com.kingsware.kdev.core.orm.DB;
 import com.kingsware.kdev.core.orm.DBConnectConfig;
@@ -60,6 +61,37 @@ public class KFaasInitialize implements SystemInitialize {
     @Value("${database.faas-ip:127.0.0.1}")
     private String faasIp;
 
+    @Value("${encrypt.sm4.key:k-pine@kingsware}")
+    private String sm4Key;
+
+    @Value("${encrypt.sm4.iv:2024120120991231}")
+    private String iv;
+
+
+    /**
+     * 解密数据源信息
+     * @param dataSourceInfo    数据源
+     */
+    private DataSourceInfo decryptDataSourceInfo(DataSourceInfo dataSourceInfo) {
+        if (dataSourceInfo.getUserName().startsWith("[SM4-") && dataSourceInfo.getUserName().endsWith("-SM4]")) {
+            String str0 = dataSourceInfo.getUserName().substring(5, dataSourceInfo.getUserName().length() - 5);
+            String str1 = SM4Utils.decryptData_CBC(str0, sm4Key, iv);
+            dataSourceInfo.setUserName(str1);
+        }
+        if (dataSourceInfo.getPassword().startsWith("[SM4-") && dataSourceInfo.getPassword().endsWith("-SM4]")) {
+            String str0 = dataSourceInfo.getPassword().substring(5, dataSourceInfo.getPassword().length() - 5);
+            String str1 = SM4Utils.decryptData_CBC(str0, sm4Key, iv);
+            dataSourceInfo.setPassword(str1);
+        }
+        if (dataSourceInfo.getJdbcUrl().startsWith("[SM4-") && dataSourceInfo.getJdbcUrl().endsWith("-SM4]")) {
+            String str0 = dataSourceInfo.getJdbcUrl().substring(5, dataSourceInfo.getJdbcUrl().length() - 5);
+            String str1 = SM4Utils.decryptData_CBC(str0, sm4Key, iv);
+            dataSourceInfo.setJdbcUrl(str1);
+        }
+        return dataSourceInfo;
+
+    }
+
     @Override
     public void execute() {
         // 启动faas
@@ -87,7 +119,7 @@ public class KFaasInitialize implements SystemInitialize {
                         ds.setDriverClass(it.getDriverClass());
                         ds.setSourceName(it.getSourceName());
                         ds.setJson(JsonUtil.toJson(it.getJson()));
-                        targetSources.add(ds);
+                        targetSources.add(decryptDataSourceInfo(ds));
                     });
                     // 获取所有的数据源
                     List<DataSourceInfo> dataSourceInfos = DB.kdbApi().queryDataSource(new DataSourceQueryArgv());
