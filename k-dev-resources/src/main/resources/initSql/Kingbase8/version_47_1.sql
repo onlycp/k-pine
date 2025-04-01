f8f4b6fe652784acd40a213f4d27db9356a36e4e
weiyitao <weiyitao@kingsware.cn>
2025-04-01 16:48:32 +0800

    调整kingbase sql

--- k-dev-resources/src/main/resources/initSql/Kingbase8/version_47_1.sql
+++ k-dev-resources/src/main/resources/initSql/Kingbase8/version_47_1.sql
@@ -1,3 +1,3 @@
 ALTER TABLE sys_menu
     ADD active_icon VARCHAR(255) NULL;
-alter table public.sys_config add is_public integer null default 0;
+alter table sys_config add is_public integer null default 0;
