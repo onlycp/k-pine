bef3f8fa929771f9bc30c087e56b5840a64063ea
weiyitao <2763652785@qq.com>
2025-05-07 11:30:22 +0800

    新增sql

--- k-dev-resources/src/main/resources/initSql/MySql/version_72_1.sql
+++ k-dev-resources/src/main/resources/initSql/MySql/version_72_1.sql
@@ -1,1 +1,2 @@
 alter table dev_git_tag add db_schema int COMMENT '是否包含表结构和数据';
+alter table dev_git_tag add db_sourcename varchar(100) COMMENT '包含了哪些数据源的表结构和数据';
--- k-dev-resources/src/main/resources/initSql/Postgresql/version_72_1.sql
+++ k-dev-resources/src/main/resources/initSql/Postgresql/version_72_1.sql
@@ -1,2 +1,6 @@
 alter table dev_git_tag add column if not EXISTS db_schema  int ;
 COMMENT ON COLUMN dev_git_tag.db_schema IS '是否包含表结构和数据';
+
+alter table dev_git_tag add column if not EXISTS db_sourcename  varchar(100) ;
+COMMENT ON COLUMN dev_git_tag.db_sourcename IS '包含了哪些数据源的表结构和数据';
+
