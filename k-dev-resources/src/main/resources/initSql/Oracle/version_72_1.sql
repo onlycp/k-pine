ea8af3a33ee7f05cfdbaa3847c4e87ed3cc03468
weiyitao <2763652785@qq.com>
2025-05-23 18:27:49 +0800

    oracle clob模糊搜索问题

--- k-dev-resources/src/main/resources/initSql/Oracle/version_72_1.sql
+++ k-dev-resources/src/main/resources/initSql/Oracle/version_72_1.sql
@@ -3,3 +3,5 @@ COMMENT ON COLUMN dev_git_tag.db_schema IS '是否包含表结构和数据';
 
 alter table dev_git_tag add db_sourcename varchar2(100 char) ;
 COMMENT ON COLUMN dev_git_tag.db_sourcename IS '包含了哪些数据源的表结构和数据';
+
+CREATE INDEX idx_page_json_text ON dev_page_template(page_json) INDEXTYPE IS CTXSYS.CONTEXT;
