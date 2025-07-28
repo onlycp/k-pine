16cc255f0033261217d303dbfc20b4519ff68730
陈鹏 <chenp_self@126.com>
2025-07-28 17:02:26 +0800

    密码统一转为base64

--- k-dev-sys/src/main/java/com/kingsware/kdev/sys/initialize/UserPasswordLineRunner.java
+++ k-dev-sys/src/main/java/com/kingsware/kdev/sys/initialize/UserPasswordLineRunner.java
@@ -5,6 +5,7 @@ import com.kingsware.kdev.core.encrypt.EncryptWorker;
 import com.kingsware.kdev.core.orm.DB;
 import com.kingsware.kdev.core.orm.SqlWrapper;
 import com.kingsware.kdev.core.orm.expression.Op;
+import com.kingsware.kdev.core.util.StringUtils;
 import com.kingsware.kdev.sys.model.SysUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -26,13 +27,16 @@ public class UserPasswordLineRunner implements CommandLineRunner {
         String mode = SpringContext.getProperties("encrypt.mode", "base64");
         if ("base64".equalsIgnoreCase(mode)) {
             SqlWrapper sqlWrapper = new SqlWrapper("select id, username, password from sys_user where 1=1");
-            sqlWrapper.addCondition("username", Op.EQ, "chenp");
+//            sqlWrapper.addCondition("username", Op.EQ, "chenp");
             sqlWrapper.notlike("password", "enc#");
             List<SysUser> users = DB.findList(SysUser.class, sqlWrapper.getSql(), sqlWrapper.getParams().toArray());
             for (SysUser user : users) {
-                String originPassword = new String(Base64.getDecoder().decode(user.getPassword().getBytes(StandardCharsets.UTF_8)));
-                String newPassword = EncryptWorker.getInstance().encrypt(originPassword, user.getUsername());
-                DB.executeUpdateSql("update sys_user set password = ? where id = ?", newPassword, user.getId());
+                if (StringUtils.isNotEmpty(user.getPassword())) {
+                    String originPassword = new String(Base64.getDecoder().decode(user.getPassword().getBytes(StandardCharsets.UTF_8)));
+                    String newPassword = EncryptWorker.getInstance().encrypt(originPassword, user.getUsername());
+                    DB.executeUpdateSql("update sys_user set password = ? where id = ?", newPassword, user.getId());
+                }
+
             }
         }
 
